26b7d87e28199145bf700c1664b7a29f4669ecb1
MrJed385 <mrjed385fans@gmail.com>
2017-08-08 17:30:23 +0100

    Update Repo
    Repo updated

---
+++ consts.js
@@ -0,0 +1,16 @@
+module.exports = {
+    kill: {
+        id: "201976968261009408",
+        waitingRoomChannel: "235803958822699010",
+        weeklyChateesRole: "344046516308148226",
+        weeklyChatChannel: "256810173702602754",
+        interrogationRole: "344046751751208960",
+        jailRole: "344046751751208960",
+        muteRole: "344046967695081482"
+    },
+    users: {
+        vicr123: "278805875978928128",
+        nebble: "113060599566508032",
+		ki11er: "114465624821989376"
+    }
+}
